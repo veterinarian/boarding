e229ec76904c6b43c740987c8248e70f5a0791a7
John Duncan <John Duncan>
2016-10-14 09:55:41 -0400

    Calendar grid now made from bookings, better understanding of dates.

--- Boarding.elm
+++ Boarding.elm
@@ -5,7 +5,11 @@ import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import Date exposing (Date, fromString, day)
+import Time
 import Date.Duration exposing (Duration(Day))
+import Maybe exposing (Maybe(Just, Nothing))
+import Date.Compare exposing (is, Compare2(After), is3, Compare3(BetweenOpen))
+import List.Extra exposing (unfoldr)
 
 main =
   start
@@ -14,52 +18,91 @@ main =
     , view = view
     }
 
-type Action = Inc | Dec
+type Action = NoAction
+            | AddBooking Booking
+            | DeleteBooking Booking
+
 update _ model = model
 
-type alias Booking = { date: Result String Date, filled: Int }
+type alias Booking = { name: String, fromDate: Date, toDate: Date }
 
 -- number of runs followed by a list of dates and their occupancy
 type alias Model = { runs: Int
-                   , dates: List Booking
+                   , bookings: List Booking
+                   , from: Maybe Date
+                   , to: Maybe Date
+                   , cal: List Date
                    }
+
+testBookings : List Booking
+testBookings =
+  [ {name = "A", fromDate = (dateFromString "10/11/2016"),
+     toDate = (dateFromString "10/14/2016")}
+  , {name = "B", fromDate = (dateFromString "10/04/2016"),
+     toDate = (dateFromString "10/13/2016")}
+  , {name = "C", fromDate = (dateFromString "10/12/2016"),
+     toDate = (dateFromString "10/19/2016")}
+  ]
 
 model : Model
 model = { runs = 5
-        , dates = [ { date = Date.fromString("2016-03-31"), filled = 2 }
-                  , { date = Date.fromString("2016-04-01"), filled = 3 }
-                  , { date = Date.fromString("2016-04-02"), filled = 3 }
-                  , { date = Date.fromString("2016-04-03"), filled = 2 }
-                  , { date = Date.fromString("2016-04-04"), filled = 0 }
-                  , { date = Date.fromString("2016-04-05"), filled = 1 }
-                  ]
+        , bookings = testBookings
+        , from = Nothing
+        , to = Nothing
+        , cal = makeCalFromStrings "10/10/2016" "10/16/2016"
         }
 
-dayString resdate =
-  case resdate of
-    Ok date -> toString (Date.day date)
-    Err date -> "-"
+dateFromString : String -> Date
+dateFromString date =
+  case Date.fromString date of
+    Ok d -> d
+    Err _ -> Date.fromTime 0
 
-avail x n = td [ class (if x.filled >= n then "occupied" else "available") ] []
+incr : Date -> Date
+incr d = Date.fromTime (Date.toTime d + (24 * Time.hour))
 
-rooms : Int -> List Booking -> List Html 
-rooms n dates =
-  let r n dates = 
+makeCalFromStrings : String -> String -> List Date
+makeCalFromStrings fs ts =
+  makeCalFromDates (dateFromString fs) (dateFromString ts)
+
+
+makeCalFromDates : Date -> Date -> List Date
+makeCalFromDates dateFrom dateTo =
+  unfoldr (\d -> if (is After d dateTo) then Nothing else Just (d, incr d))
+          dateFrom
+
+dayString date = toString (Date.day date)
+
+avail : Int -> (Date,Int) -> Html
+avail n (_,numOccupied)  =
+  td [ if n > numOccupied then class "available" else class "occupied" ] []
+
+rooms : Int -> List (Date,Int) -> List Html
+rooms numRooms occupancy =
+  let r n occupancy =
         case n of
           0 -> []
-          _ -> (tr [ class "row" ] (List.map (\x -> avail x n) dates)) :: (r (n-1) dates)
-  in List.reverse (r n dates)
+          _ -> (tr [ class "row" ] (List.map (\occ -> avail n occ) occupancy))
+               ::(r (n-1) occupancy)
+  in List.reverse (r numRooms occupancy)
+
+occupancyOf model =
+  let occupancyOn date =
+    ( date
+    , List.length (List.filter (\bk -> is3 BetweenOpen date bk.fromDate bk.toDate)
+                     model.bookings)
+    )
+  in List.map occupancyOn model.cal
 
 view : Signal.Address Action -> Model -> Html
-view address model = 
+view address model =
   div []
     [ table [ class "cal" ]
         ((tr [ class "hdr" ]
                (List.map
-                      (\booking ->
-                         td [] [ text (dayString booking.date) ])
-                      model.dates))
-        :: rooms model.runs model.dates)
-    --button [ onClick address Dec ] [ text "-" ]      
+                      (\date ->
+                         td [] [ text (dayString date) ])
+                      model.cal))
+        :: rooms model.runs (occupancyOf model))
     ]
-  
+
